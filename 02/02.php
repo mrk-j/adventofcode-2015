06272e84e1129da3225617c44fd9998dfdb7c084
Mark <mark@markj.nl>
2015-12-12 13:30:10 +0100

    Day 2 / Part 2

--- 02/02.php
+++ 02/02.php
@@ -2,6 +2,7 @@
 $input = file_get_contents('input.txt');
 $packages = explode("\n", $input);
 $paper = 0;
+$ribbon = 0;
 
 foreach($packages as $package)
 {
@@ -15,7 +16,15 @@ foreach($packages as $package)
 		$l * $h
 	];
 
+	$distance = [
+		$l + $w,
+		$w + $h,
+		$l + $h
+	];
+
 	$paper += array_sum($sides) * 2 + min($sides);
+	$ribbon += ($l * $w *$h) + min($distance) * 2;
 }
 
 echo 'The elves should order ' . $paper . ' square feet of wrapping paper' . PHP_EOL;
+echo 'They should also order ' . $ribbon . ' feet of ribbon' . PHP_EOL;
