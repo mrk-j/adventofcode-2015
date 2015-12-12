614f009c6470827a4232e10ba1e6c1dff8ddba77
Mark <mark@markj.nl>
2015-12-12 13:17:16 +0100

    Day 1 / Part 2

--- 01/01.php
+++ 01/01.php
@@ -1,10 +1,18 @@
 <?php
 $input = file_get_contents('input.txt');
 $floor = 0;
+$wasInBasement = false;
 
 for($i = 0; $i < strlen($input); $i++)
 {
 	$floor += $input{$i} === '(' ? 1 : -1;
+
+	if($floor === -1 && !$wasInBasement)
+	{
+		$wasInBasement = true;
+
+		echo 'Santa is in basement after position ' . ($i + 1) . PHP_EOL;
+	}
 }
 
 echo 'Santa is on floor ' . $floor . PHP_EOL;
