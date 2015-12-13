3f5cc6636d1efa38dee9dee532990eecf95d1ba9
Mark <mark@markj.nl>
2015-12-13 09:06:33 +0100

    Day 4 / Part 2

--- 04/04.php
+++ 04/04.php
@@ -18,3 +18,24 @@ while(!$hashFound)
 }
 
 echo 'Hash (' . $hash . ') found at number ' . $i . PHP_EOL;
+
+// Part 2
+
+$hashFound = false;
+$i = 1;
+
+while(!$hashFound)
+{
+	$hash = md5($input . $i);
+
+	if(substr($hash, 0, 6) === '000000')
+	{
+		$hashFound = true;
+	}
+	else
+	{
+		$i++;
+	}
+}
+
+echo 'Hash (' . $hash . ') found at number ' . $i . PHP_EOL;
