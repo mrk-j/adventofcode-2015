d06c944687ea88410a82e89fa6b2663d94839eb5
Mark <mark@markj.nl>
2015-12-13 17:08:51 +0100

    Day 5 / Part 2

--- 05/05.php
+++ 05/05.php
@@ -2,19 +2,19 @@
 $input = file_get_contents('input.txt');
 $strings = explode("\n", $input);
 
-function twoInARow($string)
+function twoInARow($string, $offset = 1)
 {
 	$length = strlen($string);
 
-    for($i = 0; $i < $length; $i++)
-    {
-        if(isset($string[$i + 1]) && $string[$i] == $string[$i + 1])
-        {
-        	return true;
-        }
-    }
+	for($i = 0; $i < $length; $i++)
+	{
+		if(isset($string[$i + $offset]) && $string[$i] == $string[$i + $offset])
+		{
+			return true;
+		}
+	}
 
-    return false;
+	return false;
 }
 
 function isStringNice($string)
@@ -36,5 +36,40 @@ function isStringNice($string)
 }
 
 $result = array_map("isStringNice", $strings);
+
+echo 'There are ' . array_count_values($result)[1] . ' nice strings' . PHP_EOL;
+
+function doublePair($string)
+{
+	$length = strlen($string);
+
+	for($i = 0; $i < $length; $i++)
+	{
+		if($i + 1 < $length)
+		{
+			$pair = substr($string, $i, 2);
+			$remainder = substr($string, 0, $i) . ' ' . substr($string, $i + 2);
+
+			if(strpos($remainder, $pair) !== false)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function isStringNiceTwo($string)
+{
+	if(doublePair($string) && twoInARow($string, 2))
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+$result = array_map("isStringNiceTwo", $strings);
 
 echo 'There are ' . array_count_values($result)[1] . ' nice strings' . PHP_EOL;
