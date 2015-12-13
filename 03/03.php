52b6b01c9f2ebfd005934e66940f33fcfaeb343f
Mark <mark@markj.nl>
2015-12-13 08:51:50 +0100

    Day 3 / Part 2

--- 03/03.php
+++ 03/03.php
@@ -28,3 +28,37 @@ for($i = 0; $i < strlen($input); $i++)
 }
 
 echo 'Santa was at ' . count(array_unique($map, SORT_REGULAR)) . ' houses' . PHP_EOL;
+
+$map['santa'] = [
+	[0, 0]
+];
+
+$map['robo'] = [
+	[0, 0]
+];
+
+for($i = 0; $i < strlen($input); $i++)
+{
+	$direction = $input{$i};
+
+	$key = $i % 2 == 0 ? 'santa' : 'robo';
+
+	$lastPosition = end($map[$key]);
+
+	switch($direction)
+	{
+		case '^':
+			$map[$key][$i] = [$lastPosition[0] + 1, $lastPosition[1]];
+			break;
+		case 'v':
+			$map[$key][$i] = [$lastPosition[0] - 1, $lastPosition[1]];
+			break;
+		case '>':
+			$map[$key][$i] = [$lastPosition[0], $lastPosition[1] + 1];
+			break;
+		case '<':
+			$map[$key][$i] = [$lastPosition[0], $lastPosition[1] - 1];
+			break;
+	}
+}
+echo 'Santa and RoboSanta were at ' . count(array_unique($map['santa'] + $map['robo'], SORT_REGULAR)) . ' houses' . PHP_EOL;
