03b1957e0a5aa00945c830558617ca723f89fbb2
Mark <mark@markj.nl>
2015-12-20 09:18:52 +0100

    Day 12 / Part 2

--- 12/12.php
+++ 12/12.php
@@ -25,5 +25,44 @@ function getSum($array)
 	return $sum;
 }
 
-echo 'The sum of all numbers is ' . getSum($object) . PHP_EOL;
+$array = (array) json_decode($input);
+echo 'The sum of all numbers is ' . getSum($array) . PHP_EOL;
+
+function getSumWithoutRed($array)
+{
+	$sum = 0;
+
+	if(is_object($array))
+	{
+		foreach($array as $item)
+		{
+			if($item === 'red')
+			{
+				return 0;
+			}
+		}
+	}
+
+	foreach($array as $item)
+	{
+		if(is_int($item))
+		{
+			$sum += $item;
+		}		
+		elseif(is_array($item))
+		{
+			$sum += getSumWithoutRed($item);
+		}
+		elseif(is_object($item))
+		{
+			$sum += getSumWithoutRed($item);
+		}
+	}
+
+	return $sum;
+}
+
+$object = json_decode($input);
+
+echo 'The sum of all numbers without red is ' . getSumWithoutRed($object) . PHP_EOL;
 ?>
